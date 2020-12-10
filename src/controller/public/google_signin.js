658d5ce21229e4533e7ccc4f5f49716483ffd3a3
sebin <sebin.vincent@litmus7.com>
2020-12-10 10:17:26 +0530

    Add googleTokenId request body validation

--- src/controller/public/google_signin.js
+++ src/controller/public/google_signin.js
@@ -1,58 +1,61 @@
 const { OAuth2Client } = require("google-auth-library");
-const Joi=require('joi');
-const User=require('../../model/user');
+const Joi = require('joi');
+const User = require('../../model/user');
 
 
-const authClient=new OAuth2Client("238080994627-h3gm4bjd7b617peas272a3t72eaomsbi.apps.googleusercontent.com");
+const authClient = new OAuth2Client("238080994627-h3gm4bjd7b617peas272a3t72eaomsbi.apps.googleusercontent.com");
 
 
-const loginRequestSchema=Joi.object({
-    tokenId:Joi.string().required()
+const loginRequestSchema = Joi.object({
+    tokenId: Joi.string().required()
 });
-exports.login=async (req,resp)=>{
+exports.login = async (req, resp) => {
     let user;
 
 
-    const requestBody=await loginRequestSchema.validateAsync(req.body);
-    const tokenId=requestBody.tokenId;
-    
-    
-    try{
-        var googleResponse=await authClient.verifyIdToken({
-            idToken:tokenId,
-            audience:"238080994627-h3gm4bjd7b617peas272a3t72eaomsbi.apps.googleusercontent.com"
+    try {
+        const requestBody = await loginRequestSchema.validateAsync(req.body);
+        const tokenId = requestBody.tokenId;
+        var googleResponse = await authClient.verifyIdToken({
+            idToken: tokenId,
+            audience: "238080994627-h3gm4bjd7b617peas272a3t72eaomsbi.apps.googleusercontent.com"
         });
-    }catch(ex){
+    } catch (ex) {
         console.log(ex);
         return resp.status(400).send("Bad Request");
     }
-    
-
-    let payload=googleResponse.getPayload();
 
 
-    if(payload.email_verified || (!payload.email.includes('@litmus7.com'))){
+    let payload = googleResponse.getPayload();
 
-        let email=payload.email;
-        user=await User.findOne({email:email});
-        if(user){
-            let accessToken=user.generateAuthToken('accessToken');
-            let refreshToken=user.generateAuthToken('refreshToken');
-            resp.send({accessToken,refreshToken});
-        }else{
-            user=new User({
-                email:email,
-                name:payload.name,
-                enabled:true,
-                role:'user'
+
+    if (payload.email_verified && (payload.email.includes('@litmus7.com'))) {
+
+        let email = payload.email;
+        user = await User.findOne({ email: email });
+        if (user) {
+            let accessToken = user.generateAuthToken('accessToken');
+            let refreshToken = user.generateAuthToken('refreshToken');
+            resp.send({ accessToken, refreshToken });
+        } else {
+            user = new User({
+                email: email,
+                name: payload.name,
+                enabled: true,
+                role: 'user'
             });
-            user =await user.save();
-            let accessToken=user.generateAuthToken('accessToken');
-            let refreshToken=user.generateAuthToken('refreshToken');
-            resp.send({accessToken,refreshToken});
+            user = await user.save();
+            let accessToken = user.generateAuthToken('accessToken');
+            let refreshToken = user.generateAuthToken('refreshToken');
+            resp.send({ accessToken, refreshToken });
         }
-    }else{
-        resp.status(403).send("Forbidden");
+    } else {
+
+        if (!payload.email.includes('@litmus7.com')) {
+            return resp.status(400).send("Login with Litmus7 email")
+        }
+
+        return resp.status(403).send("Forbidden");
     }
 
 
